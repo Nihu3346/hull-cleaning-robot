9ba0b9793919ffdb56c8bf656c98d225100cfb6e
Yash Mangukiya <yash.mangukiya@CDSYS.LOCAL>
2023-05-14 21:46:51 +0530

    Footer comnonent improved

--- src/Components/Navbar/Footer.js
+++ src/Components/Navbar/Footer.js
@@ -1,10 +1,19 @@
 import React from "react";
-import { Box, Container, Grid, Typography, Divider, IconButton } from "@mui/material";
+import {
+  Box,
+  Container,
+  Grid,
+  Typography,
+  Divider,
+  IconButton,
+} from "@mui/material";
 import AdbIcon from "@mui/icons-material/Adb";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import TwitterIcon from "@mui/icons-material/Twitter";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import BusinessIcon from "@mui/icons-material/Business";
+import CallIcon from "@mui/icons-material/Call";
 
 const Footer = () => {
   return (
@@ -27,6 +36,7 @@ const Footer = () => {
               justifyContent="center"
               item
               xs={12}
+              sx={{ my: 2 }}
             >
               <AdbIcon sx={{ color: "black" }} />
               <Typography
@@ -44,11 +54,13 @@ const Footer = () => {
                 LOGO
               </Typography>
             </Grid>
-            <Grid item xs={12}>
-              <Typography color="black" variant="subtitle1">
-                {`React | Material UI | React Router`}
+
+            <Grid item xs={12} sx={{ marginTop: 2 }}>
+              <Typography color="black" variant="subtitle1" style={{ fontWeight: 700 }}>
+                {`Connect`}
               </Typography>
             </Grid>
+
             <Grid item xs={12}>
               <IconButton sx={{ mr: 2 }}>
                 <FacebookIcon />
@@ -63,11 +75,47 @@ const Footer = () => {
                 <LinkedInIcon />
               </IconButton>
             </Grid>
+
+            <Grid container sx={{ marginTop: 4 }}>
+              <Grid item xs={12} md={4}>
+                <Typography variant="body1" align="center">
+                  <p style={{ fontWeight: 700 }}>Hull Cleaning</p>
+                  <p>Economic Advantage</p>
+                  <p>Environmental Advantage</p>
+                  <p>Quality Policy</p>
+                </Typography>
+              </Grid>
+              <Grid item xs={12} md={4}>
+                <Typography variant="body1" align="center">
+                  <p style={{ fontWeight: 700 }}>Quick Links</p>
+                  <p>About Us</p>
+                  <p>Out Team</p>
+                </Typography>
+              </Grid>
+              <Grid item xs={12} md={4}>
+                <Typography variant="body1" align="center">
+                  <p style={{ fontWeight: 700 }}>Contact Info</p>
+                  <p>
+                    <IconButton style={{ pointerEvents: "none" }}>
+                      <BusinessIcon sx={{ mr: 1 }} />
+                    </IconButton>
+                    Hyderabad, Jubilee Hills
+                  </p>
+                  <p>
+                    <IconButton style={{ pointerEvents: "none" }}>
+                      <CallIcon sx={{ mr: 1 }} />
+                    </IconButton>
+                    99999 99999
+                  </p>
+                </Typography>
+              </Grid>
+            </Grid>
+
             <Grid item xs={12}>
               <Typography
                 color="textSecondary"
                 variant="subtitle1"
-                sx={{ fontSize: "12px" }}
+                sx={{ fontSize: "15px" }}
               >
                 {`${new Date().getFullYear()} Company Name. All rights reserved.`}
               </Typography>
