125ac16f1eec1dc7a00a03437af4e40cb189006e
Nihar Markana <nihar.markana@CDSYS.LOCAL>
2023-05-16 22:36:04 +0530

    Release-1.0.1

--- src/Components/Pages/AboutUs.js
+++ src/Components/Pages/AboutUs.js
@@ -2,12 +2,15 @@ import { React } from "react";
 import "./../CSS/About.css";
 import TeamInfo from "./TeamInfo";
 import OurValues from "./OurValues";
+import InfoPanelA from "./InfoPanel-a";
 
 const AboutUS = () => {
   return (
     <>
+    <TeamInfo />
+    <InfoPanelA />
       <OurValues />
-      <TeamInfo />
+      
     </>
   );
 };
